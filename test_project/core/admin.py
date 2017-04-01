2e33ecae35f6234fddc5b71584f84347d4f78b08
Denis Stebunov <denis.stebunov@ivelum.com>
2017-04-01 14:22:49 +0300

    fix unwanted search button wrap with list filter

--- test_project/core/admin.py
+++ test_project/core/admin.py
@@ -8,3 +8,4 @@ from .models import Book
 @admin.register(Book)
 class BookAdmin(DjangoQLSearchMixin, admin.ModelAdmin):
     list_display = ('name', 'author', 'written', 'is_published')
+    list_filter = ('is_published',)
