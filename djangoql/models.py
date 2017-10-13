944b2b2a1561a5b5f5abe2a83abe1008e8e15c5c
Kovriga <kovriga.dmitry.25@gmail.com>
2017-10-14 01:58:03 +0300

    added model refference for the query model

--- djangoql/models.py
+++ djangoql/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.db import models
 from django.conf import settings
+from django.contrib.contenttypes.models import ContentType
 
 
 class Query(models.Model):
@@ -8,6 +9,7 @@ class Query(models.Model):
     query = models.TextField()
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     public = models.BooleanField(default=True)
+    model = models.ForeignKey(ContentType, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
