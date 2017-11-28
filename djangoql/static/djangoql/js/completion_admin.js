00f19ee070bd9f05291ee240d3068407e90e4a39
Kovriga <kovriga.dmitry.25@gmail.com>
2017-11-28 19:56:27 +0300

    removed wrapping search element from completetion_admin.js

--- djangoql/static/djangoql/js/completion_admin.js
+++ djangoql/static/djangoql/js/completion_admin.js
@@ -1,53 +1,28 @@
 (function (DjangoQL) {
-    'use strict';
+  'use strict';
 
-    DjangoQL.DOMReady(function () {
-        // Replace standard search input with textarea
-        var textarea,
-            queryArea,
-            saveButton,
-            viewButton,
-            queryTitleBar;
-        var input = document.querySelector('input[name=q]');
-        if (!input) {
-            return;
-        }
-        queryArea = document.createElement('div');
-        queryArea.className += 'djangoql-query-area';
-        saveButton = document.createElement('a');
-        viewButton = document.createElement('a');
-        queryTitleBar = document.createElement('div');
+  DjangoQL.DOMReady(function () {
+    // Replace standard search input with textarea
+    var textarea;
+    var input = document.querySelector('input[name=q]');
+    if (!input) {
+      return;
+    }
+    textarea = document.createElement('textarea');
+    textarea.value = input.value;
+    textarea.id = input.id;
+    textarea.name = input.name;
+    textarea.rows = 1;
+    textarea.setAttribute('maxlength', 2000);
+    input.parentNode.insertBefore(textarea, input);
+    input.parentNode.removeChild(input);
+    textarea.focus();
 
-        saveButton.id = 'save-query';
-        viewButton.id = 'view-queries';
-        saveButton.className += 'icon icon_save';
-        viewButton.className += 'icon icon_view';
-        saveButton.setAttribute('href', '#');
-        viewButton.setAttribute('href', '#');
-        queryTitleBar.id = 'query-name';
-
-
-        textarea = document.createElement('textarea');
-        textarea.value = input.value;
-        textarea.id = input.id;
-        textarea.name = input.name;
-        textarea.rows = 1;
-        textarea.setAttribute('maxlength', 2000);
-
-        queryArea.appendChild(textarea);
-        queryArea.appendChild(saveButton);
-        queryArea.appendChild(viewButton);
-        // insert before surrounding div elemen
-        input.parentNode.parentNode.insertBefore(queryTitleBar, input.parentNode);
-        input.parentNode.insertBefore(queryArea, input);
-        input.parentNode.removeChild(input);
-        textarea.focus();
-
-        DjangoQL.init({
-            introspections: 'introspect/',
-            syntaxHelp: 'djangoql-syntax/',
-            selector: 'textarea[name=q]',
-            autoResize: true
-        });
+    DjangoQL.init({
+      introspections: 'introspect/',
+      syntaxHelp: 'djangoql-syntax/',
+      selector: 'textarea[name=q]',
+      autoResize: true
     });
+  });
 }(window.DjangoQL));
