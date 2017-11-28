83f2275b100e3bc345df74b2b30cb0bc626a9fd6
Kovriga <kovriga.dmitry.25@gmail.com>
2017-11-28 20:03:38 +0300

    wrapped searchbar and created view and save buttons

--- djangoql/static/djangoql/js/save_queries.js
+++ djangoql/static/djangoql/js/save_queries.js
@@ -1,102 +1,56 @@
+var django;
 (function($) {
-    "use strict";
-    var formTemplate = '' +
-      '<div class="save-query-form">\n' +
-      '    <form id="save-query-form">\n' +
-      '        <div>\n' +
-      '            <label for="query-name">Name:</label>\n' +
-      '            <input type="text" id="query-name" name="query_name">\n' +
-      '        </div>\n' +
-      '        <div>\n' +
-      '            <label for="query-public">Public:</label>\n' +
-      '            <input type="checkbox" id="query-public" name="query_public">\n' +
-      '        </div>\n' +
-      '        <div>\n' +
-      '            <button type="submit">Save</button><button type="submit">Undo</button>\n' +
-      '        </div>\n' +
-      '    </form>\n' +
-      '</div>\n',
+  "use strict";
+  var URL = 'query/';
 
-      listTemplate = '' +
-        '<div class="query-list">\n' +
-        '    <div>\n' +
-        '        <input type="search" placeholder="Find by name or query" id="find-query-input">\n' +
-        '    </div>\n' +
-        '    <div>\n' +
-        '        <ul id="query-list">\n' +
-        '            \n' +
-        '        </ul>\n' +
-        '    </div>\n' +
-        '</div>',
-
-      listElementTemplate = ''
-
-      SavedQueryManager = {
-          currentQuery: {
-              query: '',
-              name: ''
-          },
-          changed: function () {
-              var currentValue = $('#searchbar').val().strip();
-              return currentValue === this.currentQuery.query;
-          },
-          save: function () {
-
-          },
-          list: function () {
-              $.getJSON('query/', function (data) {
-                  var i,
-                    queries = data.value;
-                  for (i; i < queries.length; i++){
-                      $('#query-list').append(
-
-                      )
-                  }
-              });
-          },
-          choose: function (pk) {
-
-          }
-      };
-
-    // from django docs https://docs.djangoproject.com/en/1.8/ref/csrf/
-    function getCookie(name) {
-        var cookieValue = null;
-        if (document.cookie && document.cookie !== '') {
-            var cookies = document.cookie.split(';');
-            for (var i = 0; i < cookies.length; i++) {
-                var cookie = $.trim(cookies[i]);
-                // Does this cookie string begin with the name we want?
-                if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                    break;
-                }
-            }
+  // from django docs https://docs.djangoproject.com/en/1.8/ref/csrf/
+  function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+      var i,
+        cookie,
+        cookies = document.cookie.split(';');
+      for (i = 0; i < cookies.length; i++) {
+        cookie = $.trim(cookies[i]);
+        // Does this cookie string begin with the name we want?
+        if (cookie.substring(0, name.length + 1) === (name + '=')) {
+          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+          break;
         }
-        return cookieValue;
+      }
     }
+    return cookieValue;
+  }
 
-    $(document).ready(function() {
-        $.ajaxSetup({
-            headers: { "X-CSRFToken": getCookie('csrftoken')}
-        });
-
-        $('#searchbar').change(function () {
-            if (SavedQueryManager.changed()) {
-
-            }
-        });
-
-        $('#save-query').click(function (e) {
-            e.preventDefault();
-            SavedQueryManager.saveQuery();
-        });
-
-        $('#view-queries').click(function (e) {
-            e.preventDefault();
-            SavedQueryManager.listQueries();
-        });
-
+  $(function () {
+    $.ajaxSetup({
+      headers: {'X-CSRFToken': getCookie('csrftoken')}
     });
 
+    $('#searchbar')
+        .wrap('<div class="djangoql-query-area"></div>')
+        .parent()
+        .prepend('' +
+          '<div class="query-name-bar">' +
+          '    <a href="#" id="edit-name" class="changelink">' +
+          '      Query name' +
+          '    </a>' +
+          '    <input type="text" id="query-name" placeholder="Enter query name">' +
+          '</div>')
+        .prepend('<label for="query-public">Public: </label><input type="checkbox" id="query-public" title="Public">')
+        .append('<a id="save-query" class="addlink icon" href="#" title="Save query"></a>')
+        .append('<a id="view-queries" class="viewlink icon" href="#" title="View queries"></a>')
+        .append('' +
+          '<div class="search-queries">' +
+          '    <div>' +
+          '        <input type="search" placeholder="Search by name or by query" id="find-query-input">' +
+          '    </div>' +
+          '    <div>' +
+          '        <ul id="query-list">' +
+          '            ' +
+          '        </ul>' +
+          '    </div>' +
+          '</div>'
+        );
+  });
 })(django.jQuery);
