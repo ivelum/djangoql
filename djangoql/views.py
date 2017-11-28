1fb535ef2f8b236e7b8dcab7a9784d269435f985
Kovriga <kovriga.dmitry.25@gmail.com>
2017-11-28 19:52:19 +0300

    changed query create nad update logic, both is done by POST now depending on whether query with posted name exists

--- djangoql/views.py
+++ djangoql/views.py
@@ -1,7 +1,6 @@
-from django.views.generic import TemplateView
 from django.views.generic import View
 from django.core.exceptions import ObjectDoesNotExist
-from django.http import Http404, JsonResponse
+from django.http import JsonResponse
 from django.contrib.contenttypes.models import ContentType
 from django.db.models import Q
 
@@ -12,47 +11,23 @@ from .forms import QueryForm
 class QueryView(View):
     model = None
 
-    @property
-    def _content_type(self):
-        return ContentType.objects.get(
-            app_label=self.model._meta.app_label,
-            model=self.model._meta.model_name
-        )
-
     def get(self, *args, **kwargs):
         if self.request.user.is_superuser:
             data = Query.objects.filter(model=self._content_type)
         else:
             data = Query.objects.filter(Q(user=self.request.user) | Q(public=True), model=self._content_type)
-        return JsonResponse({'value': map(dict, data.values_list('id', 'name', 'query'))})
+        return JsonResponse({'value': list(data.values('id', 'name', 'query', 'public'))})
 
     def post(self, *args, **kwargs):
-        filters = {'id': self.kwargs['pk']}
-        if not self.request.user.is_superuser:
-            filters.update({'user': self.request.user})
-        try:
-            instance = Query.objects.get(**filters)
-            form = QueryForm(data=self.request.POST, instance=instance)
-            try:
-                form.save()
-                response = {'ok': True}
-            except ValueError:
-                response = {'ok': False, 'errors': form.errors}
-        except ObjectDoesNotExist:
-            response = {'ok': False, 'errors': 'Not found'}
-        return JsonResponse(response)
-
-    def put(self, *args, **kwargs):
-        form = QueryForm(data=self.request.PUT)
-        try:
-            form.save(commit=False)
-            form.user = self.request.user
-            form.model = self._content_type
-            form.save()
-            response = {'ok': True}
-        except ValueError:
-            response = {'ok': False, 'errors': form.errors}
-        return JsonResponse(response)
+        form = QueryForm(data=self.request.POST, instance=self._get_instance())
+        if form.is_valid():
+            query = form.save(commit=False)
+            query.user = self.request.user
+            query.model = self._content_type
+            query.save()
+            return JsonResponse({'ok': True, 'id': query.id})
+        else:
+            return JsonResponse({'ok': False, 'errors': form.errors}, status=400)
 
     def delete(self, *args, **kwargs):
         ok = False
@@ -64,6 +39,18 @@ class QueryView(View):
             ok = True
         return JsonResponse({'ok': ok})
 
+    @property
+    def _content_type(self):
+        return ContentType.objects.get_for_model(self.model)
 
-
-
+    def _get_instance(self):
+        name = self.request.POST.get('name')
+        if self.request.user.is_superuser:
+            queryset = Query.objects.filter(model=self._content_type)
+        else:
+            queryset = Query.objects.filter(Q(user=self.request.user) | Q(public=True))
+        try:
+            return queryset.get(name=name, model=self._content_type)
+        except ObjectDoesNotExist:
+            pass
+        return None
