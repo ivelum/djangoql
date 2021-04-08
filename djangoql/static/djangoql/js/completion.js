20cda2fc8a0c3f53a7b02fbaa595ae61b68f78f9
Luke Cousins <luke@viovet.co.uk>
2021-04-08 12:35:11 +0100

    Explanation text for operators (#66)
    * Explanation text for operators
    
    * Typo correction
    
    * Some operators don't need explanation & >=, >, <=, < are not needed for strings
    
    Co-authored-by: Maxim Milovanov <maxim.milovanov@ivelum.com>

--- djangoql/static/djangoql/js/completion.js
+++ djangoql/static/djangoql/js/completion.js
@@ -124,15 +124,22 @@
     return result;
   };
 
-  function suggestion(text, snippetBefore, snippetAfter) {
+  function suggestion(text, snippetBefore, snippetAfter, explanation) {
     // text is being displayed in completion box and pasted when you hit Enter.
     // snippetBefore is an optional extra text to be pasted before main text.
     // snippetAfter is an optional text to be pasted after. It may also include
     // "|" symbol to designate desired cursor position after paste.
+    if (typeof explanation !== 'undefined') {
+      var suggestionText = text + ' (' + explanation + ')';
+    } else {
+      var suggestionText = text;
+    }
+
     return {
       text: text,
       snippetBefore: snippetBefore || '',
-      snippetAfter: snippetAfter || ''
+      snippetAfter: snippetAfter || '',
+      suggestionText: suggestionText
     };
   }
 
@@ -721,7 +728,7 @@
           this.completionUL.appendChild(currentLi);
         }
         currentLi.innerHTML = this.highlight(
-            this.suggestions[i].text,
+            this.suggestions[i].suggestionText,
             this.prefix);
         if (i === this.selected) {
           currentLi.className = 'active';
@@ -1078,20 +1085,26 @@
           break;
 
         case 'comparison':
-          suggestions = ['=', '!='];
+          suggestions = ['=', ['!=', 'is not equal to']];
           snippetAfter = ' ';
           if (field && field.type !== 'bool') {
             if (['str', 'date', 'datetime'].indexOf(field.type) >= 0) {
-              suggestions.push('~');
-              suggestions.push('!~');
+              suggestions.push(['~', 'contains']);
+              suggestions.push(['!~', 'does not contain']);
               snippetAfter = ' "|"';
             } else if (field.options) {
               snippetAfter = ' "|"';
             }
-            Array.prototype.push.apply(suggestions, ['>', '>=', '<', '<=']);
+            if (field.type !== 'str') {
+              Array.prototype.push.apply(suggestions, ['>', '>=', '<', '<=']);
+            }
           }
           this.suggestions = suggestions.map(function (s) {
-            return suggestion(s, '', snippetAfter);
+            if (typeof s === 'string') {
+              return suggestion(s, '', snippetAfter);
+            } else {
+              return suggestion(s[0], '', snippetAfter, s[1]);
+            }
           });
           if (field && field.type !== 'bool') {
             if (['str', 'date', 'datetime'].indexOf(field.type) >= 0
