435597c1ee7efa2fa9ba3d20268a043900a9465d
Denis Stebunov <denis.stebunov@ivelum.com>
2021-06-15 08:22:34 +0300

    use completion widget v0.3.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ import djangoql
 
 
 def download_completion_widget():
-    src_base = 'https://github.com/ivelum/djangoql-completion/raw/0.3.1/dist/'
+    src_base = 'https://github.com/ivelum/djangoql-completion/raw/0.3.2/dist/'
     target_base = 'djangoql/static/djangoql/'
     files = {
         'completion.js': target_base + 'js/',
